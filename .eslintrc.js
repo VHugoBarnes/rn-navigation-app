f01c63472c8c3b550ea929fec83f6148255ed1f0
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-02-20 18:54:33 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "prettier/prettier": 0
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-useless-catch": ["off", "always"],
+        eqeqeq: ["error", "smart"],
+        "no-var": ["error"],
+        "react-native/no-inline-styles": 0,
+      },
+    },
+  ],
+};
---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar
+        backgroundColor="black"
+        barStyle="light-content"
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
